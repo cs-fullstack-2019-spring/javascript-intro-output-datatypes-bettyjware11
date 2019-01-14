1c9212444fa631fbd49d399ada4d8d297273da13
Kevin Yancy <kevin@code-crew.org>
2019-01-14 11:25:39 -0600

    Graded 01-14-2019 11:25:33

--- Basics3.js
+++ Basics3.js
@@ -2,4 +2,6 @@
 
 var myName =prompt("What is your name?");
 var myAge =prompt ("What is your age?");
-console.log("My name is", " ", myName +" ", "and I am", " ", myAge);
+console.log("My name is", myName, "and I am", myAge);
+
+### KEY - WOW! Very well done! You went beyond what was required, and figured out you can log multiple objects separated by commas with console.log
